6b6c068717bd9605d0be403547fb0281cc8e91ea
nguyenthilananhhn99@gmail.com <nguyenthilananhhn99@gmail.com>
2021-08-31 14:29:58 +0700

    baithuchanhso3

---
+++ BaiTap/BaiTap/Controllers/GiaiPTController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaiTap.Models;
+
+namespace BaiTap.Controllers
+{
+    public class GiaiPTController : Controller
+    {
+        // GET: GiaiPT
+        GiaiPTBac1 gpt = new GiaiPTBac1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost] 
+    }
+}
---
+++ BaiTap/BaiTap/Models/GiaiPTBac1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap.Models
+{
+    public class GiaiPTBac1
+    {
+        public double GiaiPTB1(double a,double b)
+        {
+            double x = -a / b;
+            return x;
+        }
+    }
+}
